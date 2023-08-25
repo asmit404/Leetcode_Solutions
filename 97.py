6f1c7d7f3ff1e3b66175d55e8969e5fa3e24b7b7
Asmit Singh <asmittbhardwaj@gmail.com>
2023-08-25 22:16:57 +0530

    Create 97.py

---
+++ 97.py
@@ -0,0 +1,29 @@
+"""
+Title       : 97. Interleaving String
+Difficulty  : Medium
+Author      : Asmit Singh
+Solved On   : 25 Aug 2023
+Solved Using    : Python3
+"""
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        i, j = 0, 0
+
+        @cache
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            return False
+
+        return dfs(0, 0)
+
+# Time Complexity  : O(m * n)
+# Space Complexity : O(m * n)
