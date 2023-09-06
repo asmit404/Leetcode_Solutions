c6d447c5267350732b961ec2528cb4303b318250
Asmit Singh <asmittbhardwaj@gmail.com>
2023-09-06 20:33:13 +0530

    Create 725.py

---
+++ 725.py
@@ -0,0 +1,32 @@
+"""
+Title       : 725. Split Linked List in Parts
+Difficulty  : Medium
+Author      : Asmit Singh
+Solved On   : 5 Sept 2023
+Solved Using    : Python3
+"""
+
+from typing import Optional, List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        n, cur = 0, head
+        while cur:
+            n, cur = n + 1, cur.next
+        q, r = divmod(n, k)
+        res, cur = [], head
+        for i in range(k):
+            res.append(cur)
+            for j in range(q + (i < r) - 1):
+                if cur:
+                    cur = cur.next
+            if cur:
+                cur.next, cur = None, cur.next
+        return res + [None] * (k - len(res))
+
+# Time Complexity  : O(n)
+# Space Complexity : O(k)
