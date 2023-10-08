b697f694b548b7f23fb581443dd57e6ecff20a37
asmit404 <asmittbhardwaj@gmail.com>
2023-10-08 18:58:03 +0530

    Create 1458.py

---
+++ 1458.py
@@ -0,0 +1,34 @@
+"""
+Title       : 1458. Max Dot Product of Two Subsequences
+Difficulty  : Hard
+Author      : Asmit Singh
+Solved On   : 8 Oct 2023
+Solved Using    : Python3
+"""
+
+from typing import List
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+
+        if max(nums1) < 0 and min(nums2) > 0:
+            return max(nums1) * min(nums2)
+
+        if min(nums1) > 0 and max(nums2) < 0:
+            return min(nums1) * max(nums2)
+
+        m = len(nums2) + 1
+        prev_dp = [0] * m
+        dp = [0] * m
+
+        for i in range(len(nums1) - 1, -1, -1):
+            dp = [0] * m
+            for j in range(len(nums2) - 1, -1, -1):
+                use = nums1[i] * nums2[j] + prev_dp[j + 1]
+                dp[j] = max(use, prev_dp[j], dp[j + 1])
+
+            prev_dp = dp
+
+        return dp[0]
+
+# Time Complexity  : O(n * m)
+# Space Complexity : O(m)
