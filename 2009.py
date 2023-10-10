113d0306eb339d1ad1d78ee069f1f82d45d0532d
asmit404 <asmittbhardwaj@gmail.com>
2023-10-10 20:01:26 +0530

    Create 2009.py

---
+++ 2009.py
@@ -0,0 +1,23 @@
+"""
+Title       : 2009. Minimum Number of Operations to Make Array Continuous
+Difficulty  : Hard
+Author      : Asmit Singh
+Solved On   : 9 Oct 2023
+Solved Using    : Python3
+"""
+
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        nLen = len(nums)
+        nums = sorted(set(nums))
+        res = lp = 0
+        for rp, num in enumerate(nums):
+            if num - nums[lp] >= nLen:
+                lp += 1
+            res = max(res, rp - lp + 1)
+        return nLen - res
+
+# Time Complexity  : O(n log n)
+# Space Complexity : O(n)
