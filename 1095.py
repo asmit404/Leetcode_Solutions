5b91726251a1ec8433ebf8845b693eca55b25044
asmit404 <asmittbhardwaj@gmail.com>
2023-10-12 15:28:44 +0530

    Updated 1095.py

--- 1095.py
+++ 1095.py
@@ -15,6 +15,16 @@ class Solution:
             self.cache[i] = mountain_arr.get(i)
         return self.cache[i]
 
+    def ternary_search(self, l, r, mountain_arr):
+        while l < r:
+            m1 = l + (r - l) // 3
+            m2 = r - (r - l) // 3
+            if self.peek(mountain_arr, m1) < self.peek(mountain_arr, m2):
+                l = m1 + 1
+            else:
+                r = m2 - 1
+        return l
+
     def binary_search_inc(self, l, r, target, mountain_arr):
         while l <= r:
             mid = (l + r) // 2
@@ -38,18 +48,12 @@ class Solution:
         return -1
 
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        l, r = 0, mountain_arr.length() - 1
-        while l < r:
-            mid = (l + r) // 2
-            if self.peek(mountain_arr, mid) < self.peek(mountain_arr, mid + 1):
-                l = mid + 1
-            else:
-                r = mid
-
-        x = self.binary_search_inc(0, l, target, mountain_arr)
+        n = mountain_arr.length()
+        peak = self.ternary_search(0, n - 1, mountain_arr)
+        x = self.binary_search_inc(0, peak, target, mountain_arr)
         if x != -1:
             return x
-        return self.binary_search_dec(l + 1, mountain_arr.length() - 1, target, mountain_arr)
+        return self.binary_search_dec(peak + 1, n - 1, target, mountain_arr)
 
 # Beat 100% submissions in runtime.
 # Time Complexity  : O(log n)
