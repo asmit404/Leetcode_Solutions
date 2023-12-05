58fb17c2f7a956b87a792db482b96d03fb3e1e2e
Asmit Singh <asmittbhardwaj@gmail.com>
2023-12-05 16:50:37 +0530

    Create 1688.py

---
+++ 1688.py
@@ -0,0 +1,15 @@
+"""
+Title       : 1688. Count of Matches in Tournament
+Difficulty  : Easy
+Author      : Asmit Singh
+Solved On   : 05 Dec 2023
+Solved Using    : Python3
+"""
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
+
+# This is the slowest solution but fuck it, it's the most easy one.
+# Time Complexity : O(1)
+# Space Complexity : O(1)
