a1bb3c7febe5c0220d7f8b0712ba8182166ed1c3
Asmit Singh <asmittbhardwaj@gmail.com>
2024-02-26 21:45:17 +0530

    Create 100.py

---
+++ 100.py
@@ -0,0 +1,21 @@
+"""
+Title       : 100. Same Tree
+Difficulty  : Easy
+Author      : Asmit Singh
+Solved On   : 26 Feb 2024
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q: return True
+        if not p or not q: return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Time Complexity: O(n)
+# Space Complexity: O(h)
